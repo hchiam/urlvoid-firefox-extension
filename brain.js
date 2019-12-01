a61b4530bbda52856c1ca1a3ae6c77c92ad54893
hchiam <hchiam@users.noreply.github.com>
2019-12-01 18:36:19 -0500

    wrap in a function

--- brain.js
+++ brain.js
@@ -1,20 +1,23 @@
 clearStorage();
+getHosts();
 
-const children = document.head.children;
-const hosts = [];
-for (let i in children) {
-  const src = children[i].attributes['src'];
-  const host = (src && src.value) ? src.value.split('/')[2] : '';
-  const hostMinusWWW = (host.startsWith('www.')) ? host.replace(/^www\./g, '') : '';
-  if (host !== '') {
-    hosts.push(host);
-    if (hostMinusWWW !== '') hosts.push(hostMinusWWW);
-    // console.log(hosts);
-    addToStorage('hosts', hosts);
+function getHosts() {
+  const hosts = [];
+  hosts.push(String(window.location).split('/')[2]);
+  const children = document.head.children;
+  for (let i in children) {
+    const src = children[i].attributes['src'];
+    const host = (src && src.value) ? src.value.split('/')[2] : '';
+    const hostMinusWWW = (host.startsWith('www.')) ? host.replace(/^www\./g, '') : '';
+    if (host !== '') {
+      hosts.push(host);
+      if (hostMinusWWW !== '') hosts.push(hostMinusWWW);
+      // console.log(hosts);
+      addToStorage('hosts', hosts);
+    }
   }
+  // addToStorage('hosts', hosts);
 }
-
-// addToStorage('hosts', hosts);
 
 function onError(error) {
   console.log(error);
