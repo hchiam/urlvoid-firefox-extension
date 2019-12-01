9459765ee36b7384c3deb0b8a6b4284372f27e24
hchiam <hchiam@users.noreply.github.com>
2019-12-01 18:22:29 -0500

    also check without www.

--- brain.js
+++ brain.js
@@ -4,10 +4,12 @@ const children = document.head.children;
 const hosts = [];
 for (let i in children) {
   const src = children[i].attributes['src'];
-  const host = (src && src.value) ? src.value.split('/')[2] : undefined;
-  if (host !== undefined) {
-    // console.log(host);
+  const host = (src && src.value) ? src.value.split('/')[2] : '';
+  const hostMinusWWW = (host.startsWith('www.')) ? host.replace(/^www\./g, '') : '';
+  if (host !== '') {
     hosts.push(host);
+    if (hostMinusWWW !== '') hosts.push(hostMinusWWW);
+    // console.log(hosts);
     addToStorage('hosts', hosts);
   }
 }
