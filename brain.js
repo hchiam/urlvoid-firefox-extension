7426fae715b3eb00c0f9028809a95eb8e28085d9
hchiam <hchiam@users.noreply.github.com>
2020-06-28 12:12:06 -0400

    1.2.0: both firefox and chrome versions (fix #3)

--- brain.js
+++ brain.js
@@ -2,12 +2,12 @@ clearStorage();
 
 function getHosts() {
   const hosts = new Set(); // use Set to keep unique values
-  getHostFromCurrentTab(hosts)
+  getHostFromCurrentTab(hosts);
   const children = document.head.children;
   addSrcAndHrefToHosts(children, hosts);
   const scripts = document.scripts;
   addSrcAndHrefToHosts(scripts, hosts);
-  addToStorage('hosts', Array.from(hosts));
+  addToStorage("hosts", Array.from(hosts));
   // console.log(hosts);
 }
 
@@ -26,19 +26,21 @@ function addSrcAndHrefToHosts(elements, hosts) {
 }
 
 function addHostToHosts(host, hosts) {
-  const noHost = (!host || host == undefined);
+  const noHost = !host || host == undefined;
   if (noHost) return;
 
   // keep part between "//" and 1st "/", and remove the final "/":
-  host = host.split('/')[2].replace(/\/$/,'');
+  host = host.split("/")[2].replace(/\/$/, "");
 
-  const invalidHost = !host || !host.includes('.');
+  const invalidHost = !host || !host.includes(".");
   const repeatHost = hosts.has(host);
   if (invalidHost || repeatHost) return;
   hosts.add(host);
 
-  const hostMinusWWW = (host.startsWith('www.')) ? host.replace(/^www\./g, '') : '';
-  if (hostMinusWWW !== '') {
+  const hostMinusWWW = host.startsWith("www.")
+    ? host.replace(/^www\./g, "")
+    : "";
+  if (hostMinusWWW !== "") {
     hosts.add(hostMinusWWW);
   }
 
@@ -48,32 +50,28 @@ function addHostToHosts(host, hosts) {
   }
 }
 
-function onError(error) {
-  console.log(error);
-}
-
 function addToStorage(key, data) {
-  browser.storage.local.set({ [key] :  data }).then(() => {
+  chrome.storage.local.set({ [key]: data }, () => {
     // console.log('addToStorage', key, data);
-  }, onError);
+  });
 }
 
 function updateStorage(key, data) {
-  browser.storage.local.get(key).then((result) => {
-    browser.storage.local.remove(key);
+  chrome.storage.local.get(key, (result) => {
+    chrome.storage.local.remove(key);
     addToStorage(key, data);
     // console.log('updateStorage', key, data);
-  }, onError);
+  });
 }
 
 function clearStorage() {
-  browser.storage.local.clear();
+  chrome.storage.local.clear();
 }
 
-// to get messages from popup.js, use this instead of browser.storage.local.get:
-browser.runtime.onMessage.addListener((results) => {
+// to get messages from popup.js, use this instead of chrome.storage.local.get:
+chrome.runtime.onMessage.addListener((results) => {
   const openNewTabs = results.openNewTabs;
   if (openNewTabs !== true) return; // just in case
   getHosts(); // get hosts now since page has already reloaded after message received
   // (let popup.js open tabs to the hosts)
-}); // browser.runtime.onMessage.addListener does not accept onError parameter
+}); // chrome.runtime.onMessage.addListener does not accept onError parameter
--- popup.js
+++ popup.js
@@ -1,43 +1,48 @@
-function onError(error) {
-  console.log(error);
-}
-
-document.getElementById('check').addEventListener('click', () => {
-  const yes = confirm('Need to refresh this page.\n\nDo you want to continue?');
+document.getElementById("check").addEventListener("click", () => {
+  const yes = confirm("Need to refresh this page.\n\nDo you want to continue?");
   if (!yes) return;
   waitingStyle();
   // send message only after reload
-  browser.tabs.reload().then(() => {
+  chrome.tabs.executeScript({ code: "window.location.reload();" }, () => {
     // for popup.js to send data to brain.js, message tabs instead of using browser.storage.local.set:
-    browser.tabs.query({
-      currentWindow: true,
-      active: true
-    }).then(sendMessageToTabs).catch(onError);
-  }, onError);
+    chrome.tabs.query(
+      {
+        currentWindow: true,
+        active: true,
+      },
+      (tabs) => {
+        sendMessageToTabs(tabs);
+      }
+    );
+  });
 });
 
+document.getElementById(
+  "version-number"
+).innerHTML = chrome.runtime.getManifest().version;
+
 function waitingStyle() {
-  document.getElementById('check').style.display = 'none';
-  document.getElementById('message').style.display = 'block';
-  document.getElementById('message').textContent = 'Just a sec...';
+  document.getElementById("check").style.display = "none";
+  document.getElementById("message").style.display = "block";
+  document.getElementById("message").textContent = "Just a sec...";
 }
 
 function sendMessageToTabs(tabs) {
-  for (let tab of tabs) {
-    // instead of using browser.storage.local.set:
-    browser.tabs.sendMessage(
-      tab.id,
-      {
-        openNewTabs: true
-      }
-    ).catch(onError);
-  }
-  // (by now, brain.js has collected all the hosts)
-  openHostsInTabs();
+  // instead of using chrome.storage.local.set:
+  chrome.tabs.sendMessage(
+    tabs[0].id,
+    {
+      openNewTabs: true,
+    },
+    () => {
+      // (by now, brain.js has collected all the hosts)
+      openHostsInTabs(); // could be in callback?
+    }
+  );
 }
 
 function openHostsInTabs() {
-  browser.storage.local.get('hosts').then((results) => {
+  chrome.storage.local.get(["hosts"], (results) => {
     const hosts = results.hosts;
     if (hosts === undefined) {
       suggestManualForFirst();
@@ -47,17 +52,19 @@ function openHostsInTabs() {
       }
     }
     window.close();
-  }, onError);
+  });
 }
 
 function suggestManualForFirst() {
-  alert("Something went wrong.\n\nTry copying the current page's URL and running a scan here: https://www.urlvoid.com");
+  alert(
+    "Something went wrong.\n\nTry copying the current page's URL and running a scan here: https://www.urlvoid.com"
+  );
   openInNewTab();
 }
 
 function openInNewTab(host) {
-  const urlToOpen = 'https://www.urlvoid.com/scan/' + (host ? host : '');
-  browser.tabs.create({
-    url: urlToOpen
+  const urlToOpen = "https://www.urlvoid.com/scan/" + (host ? host : "");
+  chrome.tabs.create({
+    url: urlToOpen,
   });
 }
