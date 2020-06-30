a4fde78de993e326e1dfdbb24de2533a64635759
hchiam <hchiam@users.noreply.github.com>
2020-06-30 19:24:27 -0400

    add comment on how to use

--- zip.sh
+++ zip.sh
@@ -1,1 +1,3 @@
+# to use this file in CLI: bash zip.sh
+
 zip -r ${PWD##*/}.zip * .eslintignore .eslintrc.js .gitignore .travis.yml -x "node_modules/*" -x "demo/*" -x zip.sh; echo; echo "created ${PWD##*/}.zip inside this folder"; echo;
