10e6c021ff5787dfac0727188c23a06572fbd447
hchiam <hchiam@users.noreply.github.com>
2021-02-28 15:56:46 -0500

    ignore more files, properly

--- zip.sh
+++ zip.sh
@@ -1,3 +1,3 @@
 # to use this file in CLI: bash zip.sh
 
-zip -r ${PWD##*/}.zip * .eslintignore .eslintrc.js .gitignore .travis.yml -x "node_modules/*" -x "demo/*" -x zip.sh; echo; echo "created ${PWD##*/}.zip inside this folder"; echo;
+zip -r ${PWD##*/}.zip * -x "node_modules/*" -x "demo/*" -x ".github/*" -x .eslintignore -x .eslintrc.js -x .gitignore -x .travis.yml -x CODEOWNERS -x contributing.md -x zip.sh; echo; echo "created ${PWD##*/}.zip inside this folder"; echo;
